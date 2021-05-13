6721716207cf1cc1ae9c048ea1c4e7547a0e1900
Bryce Eaton <bryce.eaton86@gmail.com>
2021-05-13 10:18:04 -0500

    fixed issues

--- word_gen_app/views.py
+++ word_gen_app/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import redirect, render
 from django.utils.crypto import get_random_string
 
 def index(request):
+    request.session['counter']
     return render(request,'index.html')
 
 def random_word(request):
     request.session['word'] = get_random_string(length=14)
-    counter = 0
-    request.session['counter'] = counter + 1
-    print(request.session.__dict__)
+    request.session['counter'] += 1
     return redirect('/show-word')
 
 def show_word(request):
     return render(request, "rand-word.html")
 
 def reset(request):
-    counter = 0
+    request.session['counter'] = 0
+    return redirect('/')
